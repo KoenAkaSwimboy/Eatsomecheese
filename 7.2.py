d3fe9a587e75dbe2a9b01396d9354da2d607fb04
KoenAKASwimboy <koen.schreur@student.hu.nl>
2018-09-26 10:34:07 +0200

    les 7.2 (kaartnummers)

--- 7.2.py
+++ 7.2.py
@@ -1,4 +1,8 @@
-def numChars(kaartnummers):
-    infile = open('kaartnummers.txt', 'r')
-    content =  infile.read()
-    infile.close()
+infile = open('kaartnummers.txt', 'r')
+regels = infile.readlines()
+for regel in regels:
+    zin = regel.split(' '), regel.replace('\n', '')
+    nummer = zin[0]
+    voornaam = zin[1]
+    #achternaam = zin[2]
+    print(voornaam, 'heeft klantnummer: ')
