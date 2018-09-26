17235e34188fb53dfce27e1ed90e5396d0888bda
KoenAKASwimboy <koen.schreur@student.hu.nl>
2018-09-26 11:15:30 +0200

    les 7.2(kaartnummers)

--- 7.2.py
+++ 7.2.py
@@ -1,8 +1,16 @@
 infile = open('kaartnummers.txt', 'r')
 regels = infile.readlines()
 for regel in regels:
-    zin = regel.split(' '), regel.replace('\n', '')
+    zin = regel.split(' ')
+
     nummer = zin[0]
+    nummer = nummer.strip(',')
+
     voornaam = zin[1]
-    #achternaam = zin[2]
-    print(voornaam, 'heeft klantnummer: ')
+
+    achternaam = zin[2]
+    if len(zin) >3:
+        achternaam = achternaam + ' ' + zin[3]
+    achternaam = achternaam.strip('\n')
+
+    print(voornaam, achternaam, 'heeft klantnummer: ', nummer)
